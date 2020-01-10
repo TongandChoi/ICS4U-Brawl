a6992bea52d144ba3e30d1ac2f87aa86891386d8
wontongs <59343117+wontongs@users.noreply.github.com>
2020-01-10 14:13:55 -0500

    Update FilereadTest.java

--- FilereadTest.java
+++ FilereadTest.java
@@ -9,20 +9,50 @@ public class FilereadTest {
     String line = "";
     String cvsSplitBy = ",";
     ArrayList<Fighter> fighterList = new ArrayList<Fighter>();
+    Speed fighter1  = new Speed("temp");
+    Strength fighter2 = new Strength("temp");
+    Brain fighter3= new Brain("temp");
     
     try {
       br = new BufferedReader(new FileReader(csvFile));
       while ((line = br.readLine()) != null){
         String[] csvRow = line.split(cvsSplitBy);
         
-        Fighter fighter = new Fighter(csvRow[0], csvRow[1]);
-        fighter.set_attack(Double.parseDouble(csvRow[2]));
-        fighter.set_defense(Double.parseDouble(csvRow[3]));
-        fighter.set_speed(Double.parseDouble(csvRow[4]));
-        fighter.set_defense(Double.parseDouble(csvRow[5]));
+        if (csvRow[2].equalsIgnoreCase("Speed")) {
+          fighter1.set_name(csvRow[0]);
+          fighter1.set_attack(Double.parseDouble(csvRow[2]));
+          fighter1.set_defense(Double.parseDouble(csvRow[3]));
+          fighter1.set_speed(Double.parseDouble(csvRow[4]));
+          fighter1.set_defaultHealth(Double.parseDouble(csvRow[5]));
+          fighter1.set_health(Double.parseDouble(csvRow[6]));
+          Move moveOne = new Move("temp", 0, true, 0);
+          moveOne.set_basePower(Double.parse);
+        }
         
-        System.out.println(fighter);
+        else if (csvRow[2].equalsIgnoreCase("Strength")) {
+          fighter2.set_name(csvRow[0]);
+          fighter2.set_attack(Double.parseDouble(csvRow[2]));
+          fighter2.set_defense(Double.parseDouble(csvRow[3]));
+          fighter2.set_speed(Double.parseDouble(csvRow[4]));
+          fighter2.set_defaultHealth(Double.parseDouble(csvRow[5]));
+          fighter2.set_health(Double.parseDouble(csvRow[6]));
+        }
+        
+        else if (csvRow[2].equalsIgnoreCase("Brain")) {
+          fighter3.set_name(csvRow[0]);
+          fighter3.set_attack(Double.parseDouble(csvRow[2]));
+          fighter3.set_defense(Double.parseDouble(csvRow[3]));
+          fighter3.set_speed(Double.parseDouble(csvRow[4]));
+          fighter3.set_defaultHealth(Double.parseDouble(csvRow[5]));
+          fighter3.set_health(Double.parseDouble(csvRow[6]));
+        }
+        
+        
+        System.out.println(fighter1);
+        System.out.println(fighter2);
+        System.out.println(fighter3);
         System.out.println(Arrays.toString(csvRow));
+        
       }
       
     } 
