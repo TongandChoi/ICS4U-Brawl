87803071031c0032136eb80d5a0f11885b04d0fc
AdenChoi <56207674+AdenChoi@users.noreply.github.com>
2020-01-10 13:43:31 -0500

    Update Fighter.java

--- Fighter.java
+++ Fighter.java
@@ -2,22 +2,25 @@ import java.io.*;
 import java.util.*;
 
 class Fighter{
-  private String name;
-  private String type;
-  private double attack;
-  private double defense;
-  private double speed;
-  private double health;
-  private double defaultHealth;
+  protected String name;
+  protected double attack;
+  protected double defense;
+  protected double speed;
+  protected double health;
+  protected double defaultHealth;
+  protected String type;
   ArrayList<Move> moveSet = new ArrayList<Move>();
 
-  Fighter(String name, String type){
+  Fighter(String name){
     this.name=name;
-    this.type=type;
   }
   
   public void set_attack(double attack){
     this.attack=attack;
+  }
+  
+  public void set_type(String type){
+    this.type=type;
   }
   
   public void set_defense(double defense){
@@ -60,7 +63,6 @@ class Fighter{
     return defaultHealth;
   }
   
-  
   public String get_type(){
     return type;
   }
@@ -73,18 +75,5 @@ class Fighter{
     return moveSet;
   } 
   
-  public String toString(){
-    String returnString=" ";
-    returnString="Name: " + name;
-//    returnString+="\nType: " + type;
-//    returnString+="\nHealth: " + health;
-//    returnString+="\nAttack: " + attack;
-//    returnString+="\nDefense: " + defense;
-//    returnString+="\nSpeed: " + speed;
-//    returnString+="\n" + moveSet.get(0);
-//    returnString+="\n" + moveSet.get(1);
-//    returnString+="\n" + moveSet.get(2);
-//    returnString+="\n" + moveSet.get(3);
-    return returnString;
-  }
+  
 }
