b47f2f2552acb2da335b963616a3e92393117565
AdenChoi <56207674+AdenChoi@users.noreply.github.com>
2020-01-10 13:17:26 -0500

    Add files via upload

---
+++ Strength.java
@@ -0,0 +1,26 @@
+class Strength extends Fighter{
+  private String type;
+   
+  Strength(String name){
+    super(name);
+    type="Strength";
+  }
+  
+  public String get_type(){
+    return type;
+  }
+  
+  public void frenzy(Fighter fighter, boolean check){
+    if(fighter.get_health()<fighter.get_defaultHealth()*0.4){
+      fighter.set_attack(fighter.get_attack()*2);
+      for(int i=0 ; i<fighter.get_moveSet().size(); i++){
+        fighter.get_moveSet().get(i).set_accuracy(fighter.get_moveSet().get(i).get_accuracy()-0.05);
+      }
+      System.out.println("Ability activated: Frenzy");
+      System.out.println("Attack increase by 2*");
+      System.out.println(fighter.get_name() + "'s attack is now " + fighter.get_attack());
+      System.out.println("Accuracy for all moves are decreased by 5%");
+      }
+    }
+  }
+                         
