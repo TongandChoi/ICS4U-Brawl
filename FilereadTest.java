947d19d86cad32c866baa0d7d6d570839d40ebf6
AdenChoi <56207674+AdenChoi@users.noreply.github.com>
2020-01-11 22:36:20 -0500

    Update FilereadTest.java

--- FilereadTest.java
+++ FilereadTest.java
@@ -1,23 +1,20 @@
 import java.util.*;
 import java.io.*;
 
-public class FileReadTest {
-  public ArrayList<Fighter> list = new ArrayList<Fighter>();
-  public static void main(String[] args) throws Exception {
-    
-    String csvFile = "Fighters.csv";
+public class FilereadTest {
+  FilereadTest(){
+  }
+  public ArrayList<Fighter> fileReading(File file) throws Exception{
+    ArrayList<Fighter> fighterList = new ArrayList<Fighter>();
     BufferedReader br = null;
     String line = "";
     String cvsSplitBy = ",";
-    Speed fighter1  = new Speed("temp");
-    Strength fighter2 = new Strength("temp");
-    Brain fighter3= new Brain("temp");
-    
     try {
-      br = new BufferedReader(new FileReader(csvFile));
+      br = new BufferedReader(new FileReader(file));
       while ((line = br.readLine()) != null){
         String[] csvRow = line.split(cvsSplitBy);
         if (csvRow[1].equalsIgnoreCase("Speed")) {
+          Speed fighter1  = new Speed("temp");
           fighter1.set_name(csvRow[0]);
           fighter1.set_attack(Double.parseDouble(csvRow[2]));
           fighter1.set_defense(Double.parseDouble(csvRow[3]));
@@ -26,8 +23,8 @@ public class FileReadTest {
           fighter1.set_health(Double.parseDouble(csvRow[6]));
           for(int i=0; i<4; i++){
             Move move = new Move("temp", 0, true, 0);
-            int row = 8*i+7;
-            for(int j=row; j<row+8; j++){
+            int row = 10*i+7;
+            for(int j=row; j<=(row+9); j++){
               if(j==row){
                 move.set_name(csvRow[j]);
               }
@@ -38,29 +35,33 @@ public class FileReadTest {
                 move.set_modifiers(Boolean.parseBoolean(csvRow[j]));
               }
               else if(j==row+3){
+                move.set_powerPoints(Integer.parseInt(csvRow[j]));
+              }                       
+              else if(j==row+4){
                 move.set_modifyType(csvRow[j]);
               }
-              else if(j==row+4){
+              else if(j==row+5){
                 move.set_modifyDegree(Double.parseDouble(csvRow[j]));
               }
-              else if(j==row+5){
+              else if(j==row+6){
                 move.set_modifyDegreeTwo(Double.parseDouble(csvRow[j]));
               }
-              else if(j==row+6){
+              else if(j==row+7){
                 move.set_regen(csvRow[j]);
               }
-              else if(j==row+7){
+              else if(j==row+8){
                 move.set_asleep(Boolean.parseBoolean(csvRow[j]));
               }
-              else if(j==row+8){
+              else if(j==row+9){
                 move.set_accuracy(Double.parseDouble(csvRow[j]));
               }
             }
             fighter1.addMove(move);
           }
+          fighterList.add(fighter1);
         }
-        
-        else if (csvRow[2].equalsIgnoreCase("Strength")) {
+        else if (csvRow[1].equalsIgnoreCase("Strength")) {
+          Strength fighter2 = new Strength("temp");
           fighter2.set_name(csvRow[0]);
           fighter2.set_attack(Double.parseDouble(csvRow[2]));
           fighter2.set_defense(Double.parseDouble(csvRow[3]));
@@ -69,8 +70,8 @@ public class FileReadTest {
           fighter2.set_health(Double.parseDouble(csvRow[6]));
           for(int i=0; i<4; i++){
             Move move = new Move("temp", 0, true, 0);
-            int row = 8*i+7;
-            for(int j=row; j<row+8; j++){
+            int row = 10*i+7;
+            for(int j=row; j<=(row+9); j++){
               if(j==row){
                 move.set_name(csvRow[j]);
               }
@@ -81,28 +82,33 @@ public class FileReadTest {
                 move.set_modifiers(Boolean.parseBoolean(csvRow[j]));
               }
               else if(j==row+3){
+                move.set_powerPoints(Integer.parseInt(csvRow[j]));
+              }                       
+              else if(j==row+4){
                 move.set_modifyType(csvRow[j]);
               }
-              else if(j==row+4){
+              else if(j==row+5){
                 move.set_modifyDegree(Double.parseDouble(csvRow[j]));
               }
-              else if(j==row+5){
+              else if(j==row+6){
                 move.set_modifyDegreeTwo(Double.parseDouble(csvRow[j]));
               }
-              else if(j==row+6){
+              else if(j==row+7){
                 move.set_regen(csvRow[j]);
               }
-              else if(j==row+7){
+              else if(j==row+8){
                 move.set_asleep(Boolean.parseBoolean(csvRow[j]));
               }
-              else if(j==row+8){
+              else if(j==row+9){
                 move.set_accuracy(Double.parseDouble(csvRow[j]));
               }
             }
+            fighter2.addMove(move);
           }
+          fighterList.add(fighter2);
         }
-        
-        else if (csvRow[2].equalsIgnoreCase("Brain")) {
+        else if (csvRow[1].equalsIgnoreCase("Brain")) {
+          Brain fighter3= new Brain("temp");
           fighter3.set_name(csvRow[0]);
           fighter3.set_attack(Double.parseDouble(csvRow[2]));
           fighter3.set_defense(Double.parseDouble(csvRow[3]));
@@ -110,9 +116,9 @@ public class FileReadTest {
           fighter3.set_defaultHealth(Double.parseDouble(csvRow[5]));
           fighter3.set_health(Double.parseDouble(csvRow[6]));
           for(int i=0; i<4; i++){
-            Move move = new Move("temp", 0, true, 0);
-            int row = 8*i+7;
-            for(int j=row; j<row+8; j++){
+            Move move = new Move("temp", 0, false, 0);
+            int row = 10*i+7;
+            for(int j=row; j<=(row+9); j++){
               if(j==row){
                 move.set_name(csvRow[j]);
               }
@@ -123,41 +129,36 @@ public class FileReadTest {
                 move.set_modifiers(Boolean.parseBoolean(csvRow[j]));
               }
               else if(j==row+3){
+                move.set_powerPoints(Integer.parseInt(csvRow[j]));
+              }                       
+              else if(j==row+4){
                 move.set_modifyType(csvRow[j]);
               }
-              else if(j==row+4){
+              else if(j==row+5){
                 move.set_modifyDegree(Double.parseDouble(csvRow[j]));
               }
-              else if(j==row+5){
+              else if(j==row+6){
                 move.set_modifyDegreeTwo(Double.parseDouble(csvRow[j]));
               }
-              else if(j==row+6){
+              else if(j==row+7){
                 move.set_regen(csvRow[j]);
               }
-              else if(j==row+7){
+              else if(j==row+8){
                 move.set_asleep(Boolean.parseBoolean(csvRow[j]));
               }
-              else if(j==row+8){
+              else if(j==row+9){
                 move.set_accuracy(Double.parseDouble(csvRow[j]));
               }
             }
             fighter3.addMove(move);
-            
           }
+          fighterList.add(fighter3);
         }
-        
-        System.out.println(fighter1);
-        System.out.println(fighter2);
-        System.out.println(fighter3);
-        System.out.println(Arrays.toString(csvRow));
-        
       }
-      
     } 
     catch (FileNotFoundException e) {
       e.printStackTrace();
     }
-    
+    return fighterList;
   }
-  
 }
